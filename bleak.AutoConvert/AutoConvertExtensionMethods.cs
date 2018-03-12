fe0398c4bc6e21b962535d22750abc0bc369f621
Jamal Khan <jamal.h.khan@gmail.com>
2018-03-11 22:53:47 -0400

    reorganizing project, adding comments

--- bleak.AutoConvert/AutoConvertExtensionMethods.cs
+++ bleak.AutoConvert/AutoConvertExtensionMethods.cs
@@ -7,6 +7,12 @@ namespace bleak.AutoConvert
 {
     public static class AutoConvertExtensionMethods
     {
+        /// <summary>
+        /// Converts an input string into a scalar type, such as an integer
+        /// </summary>
+        /// <returns>The convert.</returns>
+        /// <param name="input">Input.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
         public static T AutoConvert<T>(this string input)
         {
             try
@@ -14,10 +20,7 @@ namespace bleak.AutoConvert
                 TypeConverter tc = TypeDescriptor.GetConverter(typeof(T));
                 return (T)tc.ConvertFrom(input);
             }
-            catch (Exception ex)
-            {
-
-            }
+            catch { }
             var type = typeof(T);
             if (!type.IsValueType || Nullable.GetUnderlyingType(type) != null)
             {
@@ -26,11 +29,23 @@ namespace bleak.AutoConvert
             throw new ArgumentOutOfRangeException($"AutoConvert {input} to {type.Name} failed");
         }
 
+        /// <summary>
+        /// Registers the type converter. Generally, does not need to be called by a developer
+        /// </summary>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <typeparam name="TC">The 2nd type parameter.</typeparam>
         public static void RegisterTypeConverter<T, TC>() where TC : TypeConverter
         {
             TypeDescriptor.AddAttributes(typeof(T), new TypeConverterAttribute(typeof(TC)));
         }
 
+        /// <summary>
+        /// Converts a Dictionary to an Object
+        /// </summary>
+        /// <returns>The convert.</returns>
+        /// <param name="input">Input.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <remarks>Does not recurse, therefore does not AutoConvert subclasses</remarks>
         public static T AutoConvert<T>(this Dictionary<string, object> input)
         {
             if (input == null)
@@ -63,6 +78,13 @@ namespace bleak.AutoConvert
             return (T)output;
         }
 
+        /// <summary>
+        /// Converts an Object to another type with properties of the same name.
+        /// </summary>
+        /// <returns>The convert.</returns>
+        /// <param name="input">Input.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <remarks>Does not recurse, therefore does not AutoConvert subclasses</remarks>
         public static T AutoConvert<T>(this object input) where T : new()
         {
             if (input == null)
